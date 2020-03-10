32c6db083c82997e89beed6ac68ec869c187e4df
Keako <keakon89@gmail.com>
2020-03-10 11:27:31 +0300

    function optimization

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ botlog = telebot.TeleBot('1115207301:AAHyP7JM3LLjltBgyDnZfIDloG5bKOimWOk')
 logChatID = '270174742'
 link = ''
 
-def dt():
+def data_time():
     dt = datetime.datetime.now()
     h = dt.hour
     mi = dt.minute
@@ -22,68 +22,56 @@ def dt():
     return str_dt
 
 def AddLead(title, chatID, name, phone):
-    str_dt = ''
-    f = open('text.txt', 'a')
     check_phone = []
     bx24 = Bitrix24(bitrix)
     check_phone = bx24.callMethod('crm.duplicate.findbycomm',TYPE='PHONE', VALUES=[phone])
-    str_dt = dt()
-    for j in str_dt:
-        f.write(j)
-    f.write(' ')
-    for i in check_phone:
-        f.write(i)
-    f.write('\n')
-    f.close()
+    str_dt = data_time()
     if check_phone == []:
         bx24.callMethod('crm.lead.add', fields={'TITLE':title,'PHONE':[{'VALUE':('+'+phone)}]})
 #        bx24.callMethod('crm.contact.add', fields={'NAME':name,'PHONE':[{'VALUE':('+'+phone)}]})
     else:
         if checkLEAD(check_phone) !='' and checkCONTACT(check_phone) !='':
-            bot.send_message(chatID, 'Данный клиент (' + phone + ' ' + name + ') уже присутствует в базе. За лид ответственный: ' + findOtvLead(phone) + '. За контакт ответственный: ' + findOtvKont(phone) + '.')
+            bot.send_message(chatID, 'Данный клиент (' + phone + ' ' + name + ') уже присутствует в базе. За лид ответственный: ' + findResponsible(phone, bx24, 'lead') + '. За контакт ответственный: ' + findResponsible(phone, bx24, 'contact') + '.')
         elif checkLEAD(check_phone) !='' and checkCONTACT(check_phone) =='':
-            bot.send_message(chatID, 'Данный клиент (' + phone + ' ' + name + ') уже присутствует в базе. За лид ответственный: ' + findOtvLead(phone) + '.')
+            bot.send_message(chatID, 'Данный клиент (' + phone + ' ' + name + ') уже присутствует в базе. За лид ответственный: ' + findResponsible(phone, bx24, 'lead') + '.')
         elif checkLEAD(check_phone) =='' and checkCONTACT(check_phone) !='':
-            bot.send_message(chatID, 'Данный клиент (' + phone + ' ' + name + ') уже присутствует в базе. За контакт ответственный: ' + findOtvKont(phone) + '.')
+            bot.send_message(chatID, 'Данный клиент (' + phone + ' ' + name + ') уже присутствует в базе. За контакт ответственный: ' + findResponsible(phone, bx24, 'contact') + '.')
     botlog.send_message(logChatID, str_dt + ' ' + check_phone)
-#        @bot.message_handler(commands=['yes'])
-#        def Answer(bx24, title, phone):
-#            bx24.callMethod('crm.lead.add', fields={'TITLE':title,'PHONE':[{'VALUE':phone}]})
 
 
-def parsPhone(data):
-    phone = ''
-    for i in data:
-        if i == '+':
-            phone = phone + i
-        elif i == '1':
-            phone = phone + i
-        elif i == '2':
-            phone = phone + i
-        elif i == '3':
-            phone = phone + i
-        elif i == '4':
-            phone = phone + i
-        elif i == '5':
-            phone = phone + i
-        elif i == '6':
-            phone = phone + i
-        elif i == '7':
-            phone = phone + i
-        elif i == '8':
-            phone = phone + i
-        elif i == '9':
-            phone = phone + i
-        elif i == '0':
-            phone = phone + i
-    return phone
-
-def parsName(data):
-    name = ''
-    for i in data:
-        if i != '+' and i != ' ' and i != '(' and i != ')' and i != '-' and i != '1' and i != '2' and i != '3' and i != '4' and i != '5' and i != '6' and i != '7' and i != '8' and i != '9'  and i != '0':
-            name = name + i        
-    return name
+def Parser(data, parse):
+    if parse == 'phone':
+        phone = ''
+        for i in data:
+            if i == '+':
+                phone = phone + i
+            elif i == '1':
+                phone = phone + i
+            elif i == '2':
+                phone = phone + i
+            elif i == '3':
+                phone = phone + i
+            elif i == '4':
+                phone = phone + i
+            elif i == '5':
+                phone = phone + i
+            elif i == '6':
+                phone = phone + i
+            elif i == '7':
+                phone = phone + i
+            elif i == '8':
+                phone = phone + i
+            elif i == '9':
+                phone = phone + i
+            elif i == '0':
+                phone = phone + i
+        return phone
+    if parse == 'name':
+        name = ''
+        for i in data:
+            if i != '+' and i != ' ' and i != '(' and i != ')' and i != '-' and i != '1' and i != '2' and i != '3' and i != '4' and i != '5' and i != '6' and i != '7' and i != '8' and i != '9'  and i != '0':
+                name = name + i        
+        return name
 
 def checkLEAD(data):
     led = ''
@@ -101,43 +89,25 @@ def checkCONTACT(data):
     return CONTACT
 
 
-def findOtvKont(data):
+def findResponsible(phone, bx24, find):
     id_user = ''
     name = ''
-    bx24 = Bitrix24(bitrix)
-    buf = bx24.callMethod('crm.contact.list',filter={'PHONE': data}, select=["ASSIGNED_BY_ID"])
-    for i in buf:
+    if find == 'contact':
+        search = bx24.callMethod('crm.contact.list',filter={'PHONE': phone}, select=["ASSIGNED_BY_ID"])
+    elif find == 'lead':
+        search = bx24.callMethod('crm.lead.list',filter={'PHONE': data}, select=["ASSIGNED_BY_ID"])   
+    for i in search:
         for j in i:
             id_user = i['ASSIGNED_BY_ID']
             break
     if id_user == '':
         name = 'Не задан'
     else:
-        buf3 = bx24.callMethod('user.get', filter={"ID": id_user});
-        for i in buf3:
+        buf = bx24.callMethod('user.get', filter={"ID": id_user});
+        for i in buf:
             for j in i:
                 name = i['NAME']
-                name = name + ' ' + i['LAST_NAME']
-                break
-    return name
-
-def findOtvLead(data):
-    id_user = ''
-    name = ''
-    bx24 = Bitrix24(bitrix)
-    buf = bx24.callMethod('crm.lead.list',filter={'PHONE': data}, select=["ASSIGNED_BY_ID"])
-    for i in buf:
-        for j in i:
-            id_user = i['ASSIGNED_BY_ID']
-            break
-    if id_user == '':
-        name = 'Не задан'
-    else:
-        buf3 = bx24.callMethod('user.get', filter={"ID": id_user})
-        for i in buf3:
-            for j in i:
-                name = i['NAME']
-                key_exists ='LAST_NAME' in i
+                key_exists = 'LAST_NAME' in i
                 if key_exists == 1:
                     name = name + ' ' + i['LAST_NAME']
                 break
@@ -146,11 +116,10 @@ def findOtvLead(data):
 
 @bot.message_handler(content_types=['text'])
 def readTextForLead(message):
-    phone = parsPhone(message.text)
-    name = parsName(message.text)
+    phone = Parser(message.text, 'phone')
+    name = Parser(message.text, 'name')
     if name == '':
         name = phone
-
     AddLead(message.text, message.chat.id, name, phone)
     
 bot.polling()
